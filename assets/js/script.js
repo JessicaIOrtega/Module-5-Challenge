67c2bb7af25fdefb1a503d1174609bf0967ca0bc
JessicaIOrtega <jes.ortega7@gmail.com>
2023-01-26 12:41:09 -0600

    changes to backgrounds adding classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,45 @@
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D'));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+var button = document.getElementsByClassName('saveBtn');
+var description = document.getElementsByClassName('description');
+var rows = document.getElementsByClassName("row");
+var currentTime = dayjs().hour();
+
+$(document).ready(function () {
+
+  $(".time-block").each(function () {
+    let time = $(this).attr('id')
+    if (currentTime == time) {
+      $(this).addClass("present")
+
+    } else if (currentTime < time) {
+      $(this).addClass("future")
+
+    } else if (currentTime > time) {
+      $(this).addClass("past")
+    }
+  });
+
+
+
+  function saveLS() {
+    localStorage.setItem('text', description.textContent)
+  };
+  button.addEventListener('click', saveLS);
+
+});
+
+
+
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +60,8 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+
+
